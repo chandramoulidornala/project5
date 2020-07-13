1885c213e70bff69fe6d4d5daadcb085f1615650
chandramoulidornala <chandramouli.dornala@gmail.com>
2020-07-13 21:08:29 +0530

    intial commit

---
+++ project5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i,g=0,prime=0;
+            Console.WriteLine("enter any number");
+            n = Convert.ToInt16(Console.ReadLine());
+            g = n / 2;
+            for (i = 2; i <= n - 1; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("the entered number is not prime");
+                    prime = 1;
+                    break;
+                }
+            }
+                if(prime==0)
+                    Console.WriteLine("The entered numebr is prime number");
+            }
+            
+
+        }
+    }
+
